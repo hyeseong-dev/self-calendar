4d5a9e942c034f0eaec21a290d0b291e5250b81c
HYE SEONG LEE <hyeseong @dtto9.com>
2023-12-05 15:32:03 +0900

    feat: 도메인 pojo 객체 생성

---
+++ calendar-core/src/main/java/com/fastcampus/core/domain/User.java
@@ -0,0 +1,18 @@
+package com.fastcampus.core.domain;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+public class User {
+
+    private Long id;
+    private String email;
+    private String password;
+    private LocalDate birthday;
+    private LocalDate createAt;
+}
